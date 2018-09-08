212fe37359e9c26a4bab5228432bb1dadea7963d
Pranav Jain <pranavjain@Pranavs-MacBook-Pro-8.local>
2018-09-08 00:31:26 -0400

    Implemented the minimax algorithm

--- chessGame.js
+++ chessGame.js
@@ -1,5 +1,6 @@
 
 var calculateBestPossibleMove =function(game) {
+    // TODO: this will be converted to the root call of minimax algorithm
 
     bestMove = null;
     bestValue = -100000;
@@ -26,6 +27,35 @@ var calculateBestPossibleMove =function(game) {
     return bestMoveNow
     }
 }; 
+
+var minimax = function(game, depth, isMaximizingPlayer) {
+    if (depth == 0) {
+        return calculateGameValue(game.board())
+    }
+
+    var allPossibleMoves = game.ugly_moves()
+
+    if (isMaximizingPlayer) {
+        var maxVal = -Infinity
+        for(var i = 0; i < allPossibleMoves.length; i++) {
+            game.ugly_move(allPossibleMoves[i])
+            var val = minimax(game, depth - 1, false)
+            game.undo()
+            maxVal = max(val, maxVal)
+        }  
+        return maxVal
+    }
+    else {
+        var minVal = Infinity
+        for(var i = 0; i < allPossibleMoves.length; i++) {
+           game.ugly_move(allPossibleMoves[i])
+            var val = minimax(game, depth - 1, true)
+            game.undo()
+            minVal = min(minVal, val)
+        }
+        return minVal
+    }
+}
 
 var reverseArray = function(array) {
     return array.slice().reverse();
@@ -80,6 +110,18 @@ var kingTableWhite =
    [20,  30,  10,   0,   0,  10,  30,  20]
 ]
 
+var kingEndTableWhite = 
+[
+    [-50,-40,-30,-20,-20,-30,-40,-50],
+    [-30,-20,-10,  0,  0,-10,-20,-30],
+    [-30,-10, 20, 30, 30, 20,-10,-30],
+    [-30,-10, 30, 40, 40, 30,-10,-30],
+    [-30,-10, 30, 40, 40, 30,-10,-30],
+    [-30,-10, 20, 30, 30, 20,-10,-30],
+    [-30,-30,  0,  0,  0,  0,-30,-30],
+    [-50,-30,-30,-30,-30,-30,-30,-50]
+]
+
 var kingTableBlack = reverseArray(kingTableWhite)
 
 
@@ -124,18 +166,47 @@ var calculateGameValue = function(board) {
     var totalValue = 0 
     for(var i = 0; i < 8; i++) {
         for(var j = 0; j < 8; j++) {
-            totalValue += getPieceValue(board[i, j], i, j)
+            totalValue += getPieceValue(board[i, j], false, i, j)
         }
     }
     return totalValue
 }
 
-var getPieceValue = function(position, x, y) {}
+var getPieceValue = function(piece, x, y) {
+    var getAbsoluteValue = function(piece, ifWhite, x, y) {
+        switch(piece.type) {
+            case "p": return 10 + ifWhite? pawnTableWhite[x,y]: pawnTableBlack[x,y]
+            case "n": return 30 + knightTable[x, y]
+            case "b": return 30 + ifWhite? bishopTableWhite[x,y]: bishopTableBlack[x,y]
+            case "r": return 50 + ifWhite? rookTableWhite[x,y]: rookTableBlack[x,y]
+            case "q": return 90 + queenTable[x,y]
+            case "k": return 900 + ifWhite? kingTableWhite[x,y]: kingTableBlack[x,y]
+        }
+    }
+
+    var absoluteValue = getAbsoluteValue(piece, piece.type === "w", x, y)
+    return piece.type === "w" ? absoluteValue: -absoluteValue
+}
+
+var onDrop = function(source, target) {
+    
+    var move = game.move({
+        from: source,
+        to: target,
+        promotion: "q"
+    })
+
+    if (move === null) {
+        return "snapback"
+    }
+}
+
 
 var config = {
     showNotation: false,
     position: 'start',
     draggable: true,
+    onDrop: onDrop,
     moveSpeed: 'slow',
     snapbackSpeed: 500,
     snapSpeed: 100
